121efd8b35b3ee7ad7dec714e4aba43f94ad3b6c
Robert <robertkremer@protonmail.com>
2022-05-09 23:14:16 +0200

    Updated extract.py & install deps.sh Added filter functionality to extract.py Updated required dependencies in install deps.sh
    Signed-off-by: Robert <robertkremer@protonmail.com>

--- extract.py
+++ extract.py
@@ -1,45 +1,42 @@
+import os
+
 from cv2 import *
 
-capture = VideoCapture(0)
+# Exit if no positive images are given
+if not os.path.exists('positive'):
+    print('Positive images not found')
+    exit(-1)
 
-while capture.isOpened():
-    # Capture image
-    ret, frame = capture.read()
+# Open file stream to info.dat
+info = open('info.dat', 'w')
+index = 0
 
-    # Escape if capturing image failed
-    if not ret:
-        print('Failed to capture frame')
-        capture.release()
-        exit()
+# Process all jpgs in positive directory
+# TODO: Fix loop occasionally duplicating & replacing images
+for file in os.listdir('positive'):
+    if not file.endswith('.jpg'):
+        continue
+
+    # Read image from file
+    img = imread(f'positive/{file}')
+    os.remove(f'positive/{file}')
 
     # Convert to hsv and mask frame
-    converted = cvtColor(frame, COLOR_BGR2HSV)
+    converted = cvtColor(img, COLOR_BGR2HSV)
+    # TODO: Cigarette mask
     # mask = inRange(converted, (25, 200, 160), (35, 220, 180))
     mask = inRange(converted, (85, 140, 0), (140, 255, 255))
-    filtered = bitwise_and(frame, frame, mask=mask)
 
     # Find contours on mask
     contours, _ = findContours(mask, RETR_LIST, CHAIN_APPROX_SIMPLE)
-
-    # Check if a box was found
     if len(contours) == 0:
-        imshow('Masked image', frame)
-        waitKey(1)
         continue
 
-    # Draw contours of box
+    # Filter contours, get position of largest contour and delete existing image
     contours = sorted(contours, key=contourArea, reverse=True)
-    area = contourArea(contours[0])
-
-    # Check if area is greater than 0
-    if area == 0:
-        continue
-
-    # Get position of box contours and draw text
-    # drawContours(frame, contours, 0, (255, 255, 255), 3)
     x, y, width, height = boundingRect(contours[0])
-    # distance = (18 / area) * 100
 
-    putText(filtered, 'Box', (x + int(width / 4), y - 5), FONT_ITALIC, 0.5, (255, 255, 255), 3)
-    imshow('Masked image', frame[y:y + height if height > 100 else y + 100, x:x + width if width > 100 else x + 100])
-    waitKey(1)
+    # Write result to files
+    info.write(f'positive/{index}.jpg  {index}  {x} {y} {width} {height}\n')
+    imwrite(f'positive/{index}.jpg', img)
+    index += 1
--- install deps.sh
+++ install deps.sh
@@ -1,14 +1,12 @@
 # Update system and install required packages
 sudo pacman -Syu
-sudo pacman --needed -S bluez-utils opencv-python
+sudo pacman --needed -S gcc bluez bluez-utils python-pip
 
-# Install PyBluez from github because of a few crucial fixes
-pip install git+https://github.com/pybluez/pybluez
-
-# TODO: Ask to override bluetooth service
+# Install OpenCV and PyBluez from github because of a few crucial fixes
+pip install opencv-python git+https://github.com/pybluez/pybluez
 
 # Move bluetooth service override to override directory
-sudo mv -b override.conf /etc/systemd/system/bluetooth.service.d/
+# TODO: sudo mv -b override.conf /etc/systemd/system/bluetooth.service.d/
 
 # Enable bluetooth service and (re)start
 sudo systemctl enable bluetooth
