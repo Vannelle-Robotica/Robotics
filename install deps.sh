1be18c84237ddf956b1ffa60a7b16a887c07c80e
Robert <robertkremer@protonmail.com>
2022-05-31 13:21:53 +0200

    Added loadcell.py & updated install deps.sh
    Signed-off-by: Robert <robertkremer@protonmail.com>

--- install deps.sh
+++ install deps.sh
@@ -5,7 +5,7 @@ sudo apt update
 sudo apt upgrade
 
 # Install dependencies using pip
-pip install opencv-python bluepy smbus
+pip install opencv-python bluepy smbus rpi.gpio 'git+https://github.com/bytedisciple/HX711.git#egg=HX711&subdirectory=HX711_Python3'
 
 # Move bluetooth service override to override directory
 sudo mkdir -p /etc/systemd/system/bluetooth.service.d/
