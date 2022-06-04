fa3832ff3db3cd075446d1f273e1b0deff3c9f95
lukeglas <luke@nielsglas.nl>
2022-06-04 16:48:49 +0200

    peuk detectie aangepast

--- extract.py
+++ extract.py
@@ -1,8 +1,10 @@
+import math
 import os
-
 import cv2 as cv
 
 # Exit if no positive images are given
+from cv2 import drawContours, imshow, waitKey
+
 if not os.path.exists('positive'):
     print('positive images not found')
     exit(1)
@@ -10,10 +12,12 @@ if not os.path.exists('positive'):
 # Open file stream to positive.dat
 output = open('positive.dat', 'w')
 index = 0
+factor = 0
+
 
 # Process all jpgs in positive directory
 for file in os.listdir('positive'):
-    if not file.endswith('.png'):
+    if not file.endswith('.jpg'):
         continue
 
     # Read image from positive directory
@@ -22,11 +26,32 @@ for file in os.listdir('positive'):
     # Convert to hsv and mask frame
     converted = cv.cvtColor(img, cv.COLOR_BGR2HSV)
     # TODO: Cigarette mask
-    mask = cv.inRange(converted, (10, 100, 0), (28, 255, 255))#orange
+    mask = cv.inRange(converted, (10, 30, 120), (20, 255, 255))#orange
     #mask = cv.inRange(converted, (85, 140, 0), (140, 255, 255))#blue
 
     # Find contours on mask
     contours, _ = cv.findContours(mask, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
+
+    for cnr in range(len(contours)):
+        cnt = contours[cnr]
+        area = cv.contourArea(cnt)
+        perimeter = cv.arcLength(cnt, True)
+
+
+        # calculates how round a object is
+        if area > 5:
+            factor = 4 * math.pi * area / perimeter ** 2
+
+
+        if factor > 0.4:
+            exit  # cv.drawContours(img, [cnt], -1, (0, 255, 255), 3)  # yellow
+        else:
+            # print(area)
+
+            if area > 600 and area < 1100:
+                cv.drawContours(img, [cnt], -1, (0, 255, 0), 3)  # green
+
+
     if len(contours) == 0:
         continue
 
@@ -34,8 +59,14 @@ for file in os.listdir('positive'):
     contours = sorted(contours, key=cv.contourArea, reverse=True)
     x, y, width, height = cv.boundingRect(contours[0])
 
+
     # Write result to files
     output.write(f'positive/{file}  {index}  {x} {y} {width} {height}\n')
     index += 1
+
+
+
+    imshow('Extracted', img)
+    waitKey(-1)
 
 output.close()
