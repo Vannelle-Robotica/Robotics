de6c9c5995aeb863531dac10b76dfa3f3b19adf2
Robert <robertkremer@protonmail.com>
2022-06-07 14:35:15 +0200

    Updated opencv.py Improved arduino.py#to_int_list Replaced original code with function for finding blue and brown objects Removed extract.py & gen cascade.sh since it will no longer be used
    Signed-off-by: Robert <robertkremer@protonmail.com>

--- app/utils/opencv.py
+++ app/utils/opencv.py
@@ -1,68 +1,24 @@
-import math
 import cv2 as cv
 
-# Initialize video capture and image classifier
-capture = cv.VideoCapture(0)
-index = 0
-factor = 0
+# Blue square mask
+BLUE_SQUARE = [(85, 140, 0), (140, 255, 255)]
 
-while capture.isOpened():
-    # Capture image
-    ret, frame = capture.read()
+# Cigarette mask
+CIGARETTE = [(10, 100, 0), (28, 255, 255)]
 
-    # Escape if capturing image failed
-    if not ret:
-        print('Failed to capture frame')
-        capture.release()
-        exit()
 
+def get_object(frame, mask, min_size):
+    """Finds the largest object with the specified mask in the frame"""
+    # Mask all blue and brown objects in image
     converted = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
-    mask = cv.inRange(converted, (10, 75, 0), (20, 255, 255))
-    filtered = cv.bitwise_and(frame, frame, mask=mask)
-    filtered = cv.cvtColor(filtered, cv.COLOR_RGB2GRAY)
-    contours, _ = cv.findContours(filtered, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
-    __, width, ___ = capture.shape
-    width = width / 2
-    lowerWidth = width - 50
-    higherWidth = width + 50
 
-    if len(contours) > 0:
-        contours = sorted(contours, key=cv.contourArea, reverse=True)
-        contour = contours[0]
+    # Mask image and find contours
+    masked = cv.inRange(converted, mask[0], mask[1])
+    contours, _ = cv.findContours(masked, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
 
-        # Draws a Centroid in the circle with this fomula
-        M = cv.moments(contours[0])
-        cX = int(M["m10"] / M["m00"])
-        cY = int(M["m01"] / M["m00"])
+    # Sort contours by size
+    contours = sorted(contours, key=cv.contourArea, reverse=True)
 
-        # rechter wielen moeten harder rijden
-        if cX < lowerWidth:
-            exit
-        # linker wielen moeten harder rijden
-        if cX > higherWidth:
-            exit
-        # beide niet dan rechtdoor rijden
-
-        cv.circle(capture, (cX, cY), 1, (0, 0, 0), 2)
-
-        area = cv.contourArea(contour)
-        perimeter = cv.arcLength(contour, True)
-
-        # calculates how round a object is
-        if area > 5:
-            factor = 4 * math.pi * area / perimeter ** 2
-
-        # Draw bounding box
-        if factor < 0.4 and factor > 0.1:
-            if area > 200 and area < 1000:
-                approx = cv.approxPolyDP(contour, 0.009 * cv.arcLength(contour, True), True)
-                cv.drawContours(frame, [approx], 0, (255, 0, 0), 3)
-
-    cv.imshow('OpenCV', frame)
-    if cv.waitKey(50) != -1:
-        print('Captured frame')
-        cv.imwrite(f'positive/{index}.jpg', frame)
-        index += 1
-
-capture.release()
-print('Exit')
+    if len(contours) > 0 and cv.contourArea(contours[0]) > min_size:
+        return contours[0]
+    return None
