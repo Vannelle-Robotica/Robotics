820d3fcf65d43a177e41f56bc697511e8086b073
lukeglas <luke@nielsglas.nl>
2022-06-09 11:20:33 +0200

    added 2 functions that work with turning to a object

--- app/utils/opencv.py
+++ app/utils/opencv.py
@@ -1,4 +1,5 @@
 import cv2 as cv
+from app.hardware import motors
 
 # Blue square mask
 BLUE_SQUARE = [(85, 140, 0), (140, 255, 255)]
@@ -20,5 +21,50 @@ def get_object(frame, mask, min_size):
     contours = sorted(contours, key=cv.contourArea, reverse=True)
 
     if len(contours) > 0 and cv.contourArea(contours[0]) > min_size:
+        #turn_to_object(frame, contours)
+        follow_cube(frame, contours)
         return contours[0]
     return None
+
+def turn_to_object(frame, contours):
+    __, width, ___ = frame.shape
+    width = width / 2
+    lowerWidth = width - 50
+    higherWidth = width + 50
+
+    M = cv.moments(contours[0])
+    cX = int(M["m10"] / M["m00"])
+
+    # rechter wielen moeten harder rijden
+    if cX < lowerWidth:
+        motors.move(motors,"rl",2)
+
+    # linker wielen moeten harder rijden
+    if cX > higherWidth:
+        motors.move(motors,"rr",2)
+
+    # beide niet dan rechtdoor rijden
+    else:
+        motors.move(motors,"f",2)
+        cv.waitKey(3000)
+        motors.move(motors, "s", 2)
+
+
+def follow_cube(frame, contours):
+    __, width, ___ = frame.shape
+    width = width / 2
+    lowerWidth = width - 50
+    higherWidth = width + 50
+
+    M = cv.moments(contours[0])
+    cX = int(M["m10"] / M["m00"])
+
+    # rechter wielen moeten harder rijden
+    if cX < lowerWidth:
+        motors.move(motors, "rl", 2)
+
+    # linker wielen moeten harder rijden
+    if cX > higherWidth:
+        motors.move(motors, "rr", 2)
+
+
