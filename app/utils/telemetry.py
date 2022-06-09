a08c44fc8fed0143abe21d799c53bb32e6b5b132
lukeglas <luke@nielsglas.nl>
2022-06-09 15:43:14 +0200

    telemetry works, only the speed is needed

--- app/utils/telemetry.py
+++ app/utils/telemetry.py
@@ -1,4 +1,10 @@
 import requests as rq
+import time
+import psutil
+
+from app import Application
+from app.hardware import loadcell
+from app.utils import opencv
 
 url = 'http://localhost:5217/upload'
 
@@ -16,11 +22,48 @@ def get_temperature():
     file.close()
     return float(contents) / 1000
 
+def get_weight():
+    weight = loadcell.get_weight(loadcell)
+    return weight
+
+def get_speed():
+    # diameter = 105 mm
+    # diameter * pi = distance
+    distance = 0.3298 # m/s
+    start = time.perf_counter() # sec
+    #if angle == 360:
+    stop = time.perf_counter()
+    #else:
+     #   exit()
+    timebetween = stop - start
+    speed = distance /timebetween
+    return speed
+
+def get_mode():
+    mode = Application.currentMode
+
+    return mode
+
+def get_batterylvl():
+    batterylvl = psutil.sensors_battery()
+    batteryPersentage = str(batterylvl.percent)
+    return batteryPersentage
+
+def get_vacuumstatus():
+    status = False
+    if opencv.turn_to_object:
+        status = True
+
+    return status
+
 # TODO: Add more data and use actual values from sensors
 # data = {
-#     'Mode': 0,
-#     'Temperature': 90,
-#     'Weight': 10
+#     'Mode': get_mode(),
+#     'Temperature': get_temperature(),
+#     'Weight': get_weight(),
+#     'BatteryPersentage': get_batterylvl(),
+#     'Speed': get_speed(),
+#     'VacuumStatus': get_vacuumstatus()
 # }
 
 # response = rq.post(url, data)
