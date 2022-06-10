e9f58bc4c46a55fbac9b2a5e43f86090616e1622
Stevenadema28 <stevenadema18@hotmail.com>
2022-06-10 11:44:34 +0200

    Speed Calculation

--- app/utils/telemetry.py
+++ app/utils/telemetry.py
@@ -1,20 +1,19 @@
 import math
-
 import requests as rq
 import time
 import psutil
-
 from app import Application
 from app.hardware import loadcell
+from app.hardware.motors import Motors
 from app.utils import opencv
 
 url = 'http://localhost:5217/upload'
 
-
+# Upload Data
 def upload(data):
     return rq.post(url, data)
 
-
+# Get Temperature from Raspberry Pi
 def get_temperature():
     # Read temperature from thermal zone 0
     file = open('/sys/class/thermal/thermal_zone0/temp')
@@ -24,30 +23,38 @@ def get_temperature():
     file.close()
     return float(contents) / 1000
 
+# Get Weight from the loadcells
 def get_weight():
     weight = loadcell.get_weight(loadcell)
     return weight
 
+# Get speed from the servomotors
 def get_speed():
     diameter = 4.13386 # In Inches
     circumference = (diameter * math.pi) / 12 # In Feet
     revolutionsPerMile = circumference / 5280
-    wheelSpeed = 310 # RPM
-    milesPerHour = (wheelSpeed / revolutionsPerMile) * 60
-    kilometerPerHour = milesPerHour * 1,609
+    speed = Motors.speed()
+    rpmRatio = 1.5
+    maxRpm = 310 / rpmRatio # RPM of full capacity
+    currentRpm = maxRpm * speed / 255
+    milesPerHour = (revolutionsPerMile / currentRpm) * 60
+    kilometerPerHour = milesPerHour * 1.609
 
     return kilometerPerHour
 
+# Get operation mode
 def get_mode():
     mode = Application.currentMode
 
     return mode
 
+# Get battery capacity
 def get_batterylvl():
     batterylvl = psutil.sensors_battery()
     batteryPersentage = str(batterylvl.percent)
     return batteryPersentage
 
+# Get the state of the vacuumcleaner
 def get_vacuumstatus():
     status = False
     if opencv.turn_to_object:
@@ -55,15 +62,14 @@ def get_vacuumstatus():
 
     return status
 
-# TODO: Add more data and use actual values from sensors
-# data = {
-#     'Mode': get_mode(),
-#     'Temperature': get_temperature(),
-#     'Weight': get_weight(),
-#     'BatteryPersentage': get_batterylvl(),
-#     'Speed': get_speed(),
-#     'VacuumStatus': get_vacuumstatus()
-# }
+data = {
+    'Mode': get_mode(),
+    'Temperature': get_temperature(),
+    'Weight': get_weight(),
+    'BatteryPersentage': get_batterylvl(),
+    'Speed': get_speed(),
+    'VacuumStatus': get_vacuumstatus()
+}
 
-# response = rq.post(url, data)
-# print(f'Upload response({response.status_code}): {response.reason}')
+response = rq.post(url, data)
+print(f'Upload response({response.status_code}): {response.reason}')
